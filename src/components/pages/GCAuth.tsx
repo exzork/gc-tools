a5bfdcc2669594c1840766af30cdf9f89b18c577
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-05-23 19:22:35 +0700

    show token/otp

--- src/components/pages/GCAuth.tsx
+++ src/components/pages/GCAuth.tsx
@@ -272,8 +272,12 @@ export default function GCAuth() {
                                 <button
                                     className="mt-auto bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded-md w-full">Login
                                 </button>
-                                <button onClick={handleCopyToken} className="mt-2 bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded-md w-full">Copy Token
-                                </button>
+                                <input type="text" onClick={(e) => {
+                                    navigator.clipboard.writeText(e.currentTarget.value)
+                                }}
+                                       placeholder="Your token here"
+                                       className="mt-2 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                                       value={token} readOnly/>
                             </form>
                         </div>
                         <div className="col-span-1">
